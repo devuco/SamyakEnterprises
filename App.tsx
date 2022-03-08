049b362f9741df67dfd9615c52b36218a766bba8
Samyak Agrawal <samyakagrawal13@gmail.com>
2022-03-08 17:06:57 +0530

    chane

--- App.tsx
+++ App.tsx
@@ -34,7 +34,7 @@ const App = () => {
           initialParams={{icon: 'favorite'}}
         />
         <Tab.Screen
-          name="Service"
+          name="Services"
           component={Home}
           initialParams={{icon: 'handyman'}}
         />
